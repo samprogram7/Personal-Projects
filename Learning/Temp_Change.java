42d652e19494c521ea5374f6ae6001c6a2af89e4
samprogram7 <samuelr0207@icloud.com>
2023-12-25 21:22:16 -0700

    Tempeture commit

---
+++ Learning/Temp_Change.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Temp_Change {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter your Tempeture in Celcius");
+
+ 
+
+
+
+       
+
+    }
+
+
+
+}
